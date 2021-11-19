9bc09e9ba528e48adc8593435f25e0ac27064f8c
Andres Torres <atorres@applab.mx>
2021-11-19 13:36:34 -0600

    Distancia entre puntos

---
+++ DistanciaEntrePuntos/Point.h
@@ -0,0 +1,41 @@
+#include <cmath>
+
+class Point{
+    private:
+        double x;
+        double y;
+    public: 
+        Point();
+        Point(double x, double y);
+        double getX();
+        double getY();
+        void setX(double x);
+        void setY(double y);
+        double calaculateDistance(Point point2);
+};
+Point::Point(){
+    x=0.0;
+    y=0.0;
+}
+Point::Point(double x, double y){
+    this->x=x;
+    this->y=y;
+}
+double Point::getX(){
+    return x;
+}
+double Point::getY(){
+    return y;
+}
+void Point::setX(double x){
+    this->x=x;
+}
+void Point::setY(double y){
+    this->y=y;
+}
+double Point::calaculateDistance(Point point2){
+    double resultado=0;
+    resultado=sqrt(pow((x-point2.getX()),2)+pow((y-point2.getY()),2));
+    return resultado;
+}
+
---
+++ DistanciaEntrePuntos/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "Point.h"
+using namespace std;
+
+int main(){
+    Point p1;
+    Point p2(10.0,20.0);
+
+    cout<<"El punto p1 es: X: "<<p1.getX()<<" Y: "<<p1.getY()<<endl;
+    cout<<"El punto p2 es: X: "<<p2.getX()<<" Y: "<<p2.getY()<<endl;
+    cout<<"La distancia entre los dos puntos es: "<<p1.calaculateDistance(p2)<<endl;
+
+    Point p3(15.0,5.0);
+    Point p4(30.0,1.0);
+
+    cout<<"El punto p3 es: X: "<<p3.getX()<<" Y: "<<p3.getY()<<endl;
+    cout<<"El punto p4 es: X: "<<p4.getX()<<" Y: "<<p4.getY()<<endl;
+    cout<<"La distancia entre los dos puntos es: "<<p3.calaculateDistance(p4)<<endl;
+   
+    return 0;
+}
